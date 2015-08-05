dbf3a4e63495bd757456338df7ad24dbbb4f56d7
ldin <ldinka.alena@gmail.com>
2015-08-05 14:01:25 +0300

    djivo

--- app/views/home/layout.blade.php
+++ app/views/home/layout.blade.php
@@ -174,6 +174,12 @@
 <noscript><div><img src="https://mc.yandex.ru/watch/30970706" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 
+<!-- BEGIN JIVOSITE CODE {literal} -->
+<script type='text/javascript'>
+(function(){ var widget_id = 'OuVbbyLztT';
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+<!-- {/literal} END JIVOSITE CODE -->
+
 </body>
 
 </html>
