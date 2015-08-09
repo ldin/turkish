1b7a299f2969d4fdf7e0452b66d003492a2c1778
ldin <ldinka.alena@gmail.com>
2015-08-09 20:06:26 +0300

    breadcrumbs

--- app/views/home/page.blade.php
+++ app/views/home/page.blade.php
@@ -7,6 +7,19 @@
 @section('content')
 
     <div id="content" class="container">
+
+    <div class="row breadcrumbs">
+        
+        <span class="loc main" data-link="/">Организация и сопровождение бизнеса в Турции. </span>
+        @if(!empty($type->name))
+            <span class="loc page" data-link="/{{$type->type}}">{{ $type->name }}. </span>
+        @endif
+        @if(!empty($row->name))
+            <span class="loc page" data-link="/{{$row->slug}}">{{ $row->name }}. </span>
+        @endif
+
+    </div>
+
     <div class="row">
 
     <?// var_dump($posts); ?>
@@ -16,14 +29,17 @@
         @if(isset($posts)&&count($posts)>0)
             <div class="col-xs-12 col-sm-3">
 
-                <ul class="menu-page nav nav-pills nav-stacked">
+                <ul class="menu-page nav nav-pills nav-stacked ">
                     @foreach($posts as $post)
                         <li {{ (Request::is( $type->type.'/'.$post->slug)) ? 'class="active"' : '' }} >{{ HTML::link('/'.$type->type.'/'.$post->slug, $post->name) }}
+                            <?//var_dump($row->parent, $post->id)?>
                             @if(isset($posts_child)&&count($posts_child)>0)
                                 <ul>
                                     @foreach($posts_child as $post_ch)
-                                        @if($post_ch->parent == $post->id)
-                                            <li>{{ HTML::link('/'.$type->type.'/'.$post_ch->slug, $post_ch->name) }}</li>
+                                        @if(($post_ch->parent == $post->id) )
+                                            <li {{ (Request::is( $type->type.'/'.$post_ch->slug)) ? 'class="active"' : '' }}>
+                                                {{ HTML::link('/'.$type->type.'/'.$post_ch->slug, $post_ch->name) }}
+                                            </li>
                                         @endif
                                     @endforeach
                                 </ul>
