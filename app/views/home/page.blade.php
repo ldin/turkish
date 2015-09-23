597c2e6118a16c8d19cbd10177b4e25ef1da55eb
ldin <ldinka.alena@gmail.com>
2015-09-23 19:23:33 +0300

    social

--- app/views/home/page.blade.php
+++ app/views/home/page.blade.php
@@ -81,6 +81,18 @@
 
     </div>
 
+<script type="text/javascript">(function() {
+  if (window.pluso)if (typeof window.pluso.start == "function") return;
+  if (window.ifpluso==undefined) { window.ifpluso = 1;
+    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+    var h=d[g]('body')[0];
+    h.appendChild(s);
+  }})();</script>
+
+<div class="pluso pluso-txt-right" data-background="transparent" data-options="medium,square,line,horizontal,nocounter,theme=04" data-services="vkontakte,linkedin,facebook,twitter,odnoklassniki,google,moimir"></div>
+<br>
     </div>
 
 @stop
