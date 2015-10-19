cd3cd66142715eb014869fda24c0bfc87300c28d
ldin <ldinka.alena@gmail.com>
2015-10-20 00:08:54 +0300

    google analitic

--- app/views/home/layout.blade.php
+++ app/views/home/layout.blade.php
@@ -12,6 +12,17 @@
     <link href="/css/styles.css?0009" rel="stylesheet">
 
     @yield('header')
+    
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-47709808-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 
  </head>
 
@@ -218,16 +229,7 @@
 var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
 <!-- {/literal} END JIVOSITE CODE -->
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-47709808-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 
 </body>
 
